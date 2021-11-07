296a71ab9f6d7849a7a4fbce810cc56e3b7c5d87
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2021-11-07 13:44:43 -0500

    Update tag.js

--- tag.js
+++ tag.js
@@ -1,1 +1,1 @@
-window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-103640094-4');
+window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-212153848-1');
