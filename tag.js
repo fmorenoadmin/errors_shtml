41e9cd2d61e8a937ed21a35d0a4abdd3567ff579
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2025-02-18 10:59:39 -0500

    cambios

--- tag.js
+++ tag.js
@@ -1,1 +1,1 @@
-window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-212153848-1');
+window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-GJ98TVF245');
